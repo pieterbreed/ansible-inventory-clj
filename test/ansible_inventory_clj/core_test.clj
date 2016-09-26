63e50987468f72aeff33bfa5b827abb21519eaac
Pieter Breed <pieter.breed@gmail.com>
2016-09-26 15:27:21 +0200

    added the add-target-merges-vars test

--- test/ansible_inventory_clj/core_test.clj
+++ test/ansible_inventory_clj/core_test.clj
@@ -1,12 +1,41 @@
 (ns ansible-inventory-clj.core-test
   (:require [clojure.test :refer :all]
             [ansible-inventory-clj.core :refer :all]
-            [clojure.spec.gen :as gen]
-            [clojure.spec.test :as stest]
+            [clojure.test.check.generators :as tcgen]
+            [clojure.test.check.properties :as tcprop]
+            [clojure.test.check.clojure-test :as tct]
             [clojure.spec :as s]
-            [clojure.test :refer :all]))
+            ))
 
 (deftest empty-inventory-test
   (testing "That the empty inventory is in fact a valid inventory."
     (is (s/valid? :ansible-inventory-clj.core/inventory
                   ansible-inventory-clj.core/empty-inventory))))
+
+(let [target ["username" "hostname" 22]]
+  (tct/defspec add-target-merges-vars
+    100
+    (tcprop/for-all
+     [target-vars (tcgen/map tcgen/string-alphanumeric
+                             tcgen/string-alphanumeric)]
+     (let [;; expected value
+           ;; adds all of the "variables" at once
+           exp (-> empty-inventory
+                   (add-target target target-vars))
+
+           ;; test value
+           ;; add the variables one-at-a-time using the loop
+           tval (loop [keys (keys target-vars)
+                       inv (add-target empty-inventory target {})]
+                  
+                  (if (or (nil? keys)
+                          (empty? keys))
+                    inv
+                    (let [key (first keys)
+                          val (get target-vars key)]
+                      (recur (rest keys)
+                             (add-target inv target
+                                         {key val})))))]
+
+       ;; check that the result is the same
+       (= exp tval)))))
