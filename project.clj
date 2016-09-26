4b3c3e49f415ef29ca4b0e70cb4ca30eaf46096e
Pieter Breed <pieter.breed@gmail.com>
2016-09-26 16:00:28 +0200

    bumped version number for 0.2 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ansible-inventory-clj "0.1.0"
+(defproject ansible-inventory-clj "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
